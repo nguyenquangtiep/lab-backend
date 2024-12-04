47951445564c0efb0cf14efa20b134341c4b851d
tiepnq <tiepnguyenquang@tech.admicro.vn>
2024-12-05 01:06:28 +0700

    first commit

---
+++ src/test/java/com/application/lab/LabBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
